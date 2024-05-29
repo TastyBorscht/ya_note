acfdf22265eca7645c50ed16339b0bac620c79ec
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2024-05-29 17:32:32 +0300

    small changes for slug test

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
+from pytils.translit import slugify
 
 from notes.forms import NoteForm
 from notes.models import Note
@@ -11,21 +12,22 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestListPage(TestCase):  # Балк криейт.
+class TestListPage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.TEST_TITLE = 'Название заметки'
         cls.author = User.objects.create(username='Заметчик')
-        for i in range(10):
+        for i in range(3):
             cls.notes = Note.objects.create(
-                title=f'Название заметки {i}',
+                title=f'{cls.TEST_TITLE} {i}',
                 text=f'Просто текст {i}.',
                 author=cls.author
             )
 
     def test_author_got_his_slug(self):
         """ Проверка генерации slug из title."""
-        test_slugs = [f'nazvanie-zametki-{i}' for i in range(10)]
+        test_slugs = [f'{slugify(self.TEST_TITLE)}-{i}' for i in range(3)]
         self.client.force_login(self.author)
         all_slugs = [note.slug for note in self.client.get(
             reverse('notes:list')).context['object_list']]
